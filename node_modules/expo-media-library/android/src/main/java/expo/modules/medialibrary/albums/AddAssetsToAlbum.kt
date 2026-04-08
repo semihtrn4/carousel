2a2b196861586fe0e68c2be2c37a109e38741544
smhtrn10 <smhtrn10@gmail.com>
2026-04-08 19:48:58 +0300

    version22

--- node_modules/expo-media-library/android/src/main/java/expo/modules/medialibrary/albums/AddAssetsToAlbum.kt
+++ node_modules/expo-media-library/android/src/main/java/expo/modules/medialibrary/albums/AddAssetsToAlbum.kt
@@ -3,74 +3,50 @@ package expo.modules.medialibrary.albums
 import android.content.Context
 import android.media.MediaScannerConnection
 import android.os.Build
-import android.provider.MediaStore
-import expo.modules.kotlin.Promise
-import expo.modules.medialibrary.AlbumException
-import expo.modules.medialibrary.EXTERNAL_CONTENT_URI
-import expo.modules.medialibrary.MediaLibraryException
 import expo.modules.medialibrary.MediaLibraryUtils
 import expo.modules.medialibrary.PermissionsException
-import java.io.File
 import java.util.concurrent.atomic.AtomicInteger
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.withContext
 
-internal class AddAssetsToAlbum(
-  private val context: Context,
-  private val assetIds: Array<String>,
-  private val albumId: String,
-  copyToAlbum: Boolean,
-  private val promise: Promise
-) {
-  private val strategy = if (copyToAlbum) AssetFileStrategy.copyStrategy else AssetFileStrategy.moveStrategy
+suspend fun addAssetsToAlbum(
+  context: Context,
+  assetIds: Array<String>,
+  albumId: String,
+  copyToAlbum: Boolean
+): Boolean = withContext(Dispatchers.IO) {
+  val strategy = if (copyToAlbum) {
+    AssetFileStrategy.copyStrategy
+  } else {
+    AssetFileStrategy.moveStrategy
+  }
 
-  // Media store table can be corrupted. Extra check won't harm anyone.
-  private val album: File
-    get() {
-      val path = arrayOf(MediaStore.MediaColumns.DATA)
-      val selection = "${MediaStore.MediaColumns.BUCKET_ID}=?"
-      val id = arrayOf(albumId)
-      context.contentResolver.query(
-        EXTERNAL_CONTENT_URI,
-        path,
-        selection,
-        id,
-        null
-      ).use { albumCursor ->
-        if (albumCursor == null) {
-          throw AlbumException("Could not get album. Query returns null.")
-        } else if (albumCursor.count == 0) {
-          throw AlbumException("No album with id: $albumId")
-        }
-        albumCursor.moveToNext()
-        val filePathColumnIndex = albumCursor.getColumnIndex(MediaStore.Images.Media.DATA)
-        val fileInAlbum = File(albumCursor.getString(filePathColumnIndex))
+  val album = getAlbumFile(context, albumId)
+  coroutineContext.ensureActive()
 
-        // Media store table can be corrupted. Extra check won't harm anyone.
-        if (!fileInAlbum.isFile && !fileInAlbum.isDirectory) {
-          throw MediaLibraryException()
-        }
-        return File(fileInAlbum.parent!!)
-      }
-    }
+  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !album.canWrite()) {
+    throw PermissionsException(
+      "The application doesn't have permission to write to the album's directory. For more information, check out https://expo.fyi/android-r."
+    )
+  }
 
-  fun execute() {
-    val assets = MediaLibraryUtils.getAssetsById(context, *assetIds)
+  val assets = MediaLibraryUtils.getAssetsById(context, *assetIds)
+  coroutineContext.ensureActive()
 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !album.canWrite()) {
-      throw PermissionsException(
-        "The application doesn't have permission to write to the album's directory. For more information, check out https://expo.fyi/android-r."
-      )
-    }
+  val paths = assets.map { asset ->
+    val newAsset = strategy.apply(asset, album, context)
+    newAsset.path
+  }
+  coroutineContext.ensureActive()
 
-    val paths = assets.map { asset ->
-      val newAsset = strategy.apply(asset, album, context)
-      newAsset.path
-    }
-
-    val atomicInteger = AtomicInteger(paths.size)
-    MediaScannerConnection.scanFile(context, paths.toTypedArray(), null) { _, _ ->
-      if (atomicInteger.decrementAndGet() == 0) {
-        promise.resolve(true)
-      }
+  val result = CompletableDeferred<Boolean>()
+  val atomicInteger = AtomicInteger(paths.size)
+  MediaScannerConnection.scanFile(context, paths.toTypedArray(), null) { _, _ ->
+    if (atomicInteger.decrementAndGet() == 0) {
+      result.complete(true)
     }
   }
+  return@withContext result.await()
 }
--- node_modules/expo-media-library/android/src/main/java/expo/modules/medialibrary/albums/AlbumUtils.kt
+++ node_modules/expo-media-library/android/src/main/java/expo/modules/medialibrary/albums/AlbumUtils.kt
@@ -3,24 +3,30 @@ package expo.modules.medialibrary.albums
 import android.content.Context
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.MediaStore.Files.FileColumns
 import android.provider.MediaStore.MediaColumns
-import expo.modules.kotlin.Promise
+import expo.modules.core.utilities.ifNull
 import expo.modules.medialibrary.AlbumException
-import expo.modules.medialibrary.ERROR_UNABLE_TO_LOAD
-import expo.modules.medialibrary.ERROR_UNABLE_TO_LOAD_PERMISSION
+import expo.modules.medialibrary.AssetFileException
 import expo.modules.medialibrary.EXTERNAL_CONTENT_URI
+import expo.modules.medialibrary.MediaLibraryException
+import expo.modules.medialibrary.MediaLibraryUtils
 import expo.modules.medialibrary.MediaLibraryUtils.queryPlaceholdersFor
+import expo.modules.medialibrary.UnableToLoadException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.withContext
+import java.io.File
 
 /**
  * Queries for assets filtered by given `selection`.
  * Resolves `promise` with [Bundle] of kind: `Array<{ id, title, assetCount }>`
  */
-fun queryAlbum(
+suspend fun queryAlbum(
   context: Context,
   selection: String,
-  selectionArgs: Array<String>?,
-  promise: Promise
-) {
+  selectionArgs: Array<String>?
+): Bundle? = withContext(Dispatchers.IO) {
   val projection = arrayOf(MediaColumns.BUCKET_ID, MediaColumns.BUCKET_DISPLAY_NAME)
   val order = MediaColumns.BUCKET_DISPLAY_NAME
   try {
@@ -31,12 +37,12 @@ fun queryAlbum(
       selectionArgs,
       order
     ).use { albumsCursor ->
+      coroutineContext.ensureActive()
       if (albumsCursor == null) {
         throw AlbumException("Could not get album. Query is incorrect.")
       }
       if (!albumsCursor.moveToNext()) {
-        promise.resolve(null)
-        return
+        return@withContext null
       }
       val bucketIdIndex = albumsCursor.getColumnIndex(MediaColumns.BUCKET_ID)
       val bucketDisplayNameIndex = albumsCursor.getColumnIndex(MediaColumns.BUCKET_DISPLAY_NAME)
@@ -45,16 +51,12 @@ fun queryAlbum(
         putString("title", albumsCursor.getString(bucketDisplayNameIndex))
         putInt("assetCount", albumsCursor.count)
       }
-      promise.resolve(result)
+      return@withContext result
     }
   } catch (e: SecurityException) {
-    promise.reject(
-      ERROR_UNABLE_TO_LOAD_PERMISSION,
-      "Could not get albums: need READ_EXTERNAL_STORAGE permission.",
-      e
-    )
+    throw UnableToLoadException("Could not get albums: need READ_EXTERNAL_STORAGE permission ${e.message}", e)
   } catch (e: IllegalArgumentException) {
-    promise.reject(ERROR_UNABLE_TO_LOAD, "Could not get album.", e)
+    throw UnableToLoadException("Could not get album: ${e.message}", e)
   }
 }
 
@@ -82,4 +84,66 @@ fun getAssetsInAlbums(context: Context, vararg albumIds: String?): List<String> 
     }
   }
   return assetIds
+}
+
+internal suspend fun getFileOrNullByContextResolver(
+  context: Context,
+  selection: String,
+  selectionArgs: Array<String>
+): File? = withContext(Dispatchers.IO) {
+  context.contentResolver.query(
+    EXTERNAL_CONTENT_URI,
+    arrayOf(MediaColumns.DATA),
+    selection,
+    selectionArgs,
+    null
+  ).use { fileCursor ->
+    if (fileCursor == null) {
+      throw AlbumException("Could not get album. Query returns null.")
+    } else if (fileCursor.count == 0) {
+      return@withContext null
+    }
+    fileCursor.moveToNext()
+    val filePathColumnIndex = fileCursor.getColumnIndex(MediaStore.Images.Media.DATA)
+    val fileInAlbum = File(fileCursor.getString(filePathColumnIndex))
+
+    // Media store table can be corrupted. Extra check won't harm anyone.
+    if (!fileInAlbum.isFile && !fileInAlbum.isDirectory) {
+      throw MediaLibraryException()
+    }
+    return@withContext File(fileInAlbum.parent!!)
+  }
+}
+
+internal suspend fun getAlbumFileByNameOrNull(context: Context, albumName: String): File? {
+  val selection = "${FileColumns.MEDIA_TYPE} != ${FileColumns.MEDIA_TYPE_NONE} AND ${MediaColumns.BUCKET_DISPLAY_NAME}=?"
+  val selectionArgs = arrayOf(albumName)
+
+  return getFileOrNullByContextResolver(context, selection, selectionArgs)
+}
+
+internal suspend fun getAlbumFileOrNull(context: Context, albumId: String): File? {
+  val selection = "${MediaColumns.BUCKET_ID}=?"
+  val selectionArgs = arrayOf(albumId)
+
+  return getFileOrNullByContextResolver(context, selection, selectionArgs)
+}
+
+internal suspend fun getAlbumFile(context: Context, albumId: String): File {
+  val albumFile = getAlbumFileOrNull(context, albumId)
+  return albumFile ?: throw AlbumException("Could not get album. Query returns null.")
+}
+
+internal fun createAlbumFile(mimeType: String, albumName: String): File {
+  val albumDir = MediaLibraryUtils.getEnvDirectoryForAssetType(mimeType, false)
+    .ifNull {
+      throw AssetFileException("Could not guess asset type.")
+    }
+
+  val album = File(albumDir.path, albumName)
+    .takeIf { it.exists() || it.mkdirs() }
+    .ifNull {
+      throw AlbumException("Could not create album directory.")
+    }
+  return album
 }
